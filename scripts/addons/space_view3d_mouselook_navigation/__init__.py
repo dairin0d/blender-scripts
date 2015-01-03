2d4506e4cddd6c3eaffff7e39bc03f015f255e94
dairin0d <dima.glib@gmail.com>
2015-01-04 00:11:17 +0200

    Updated mouselook navigation version

--- scripts/addons/space_view3d_mouselook_navigation/__init__.py
+++ scripts/addons/space_view3d_mouselook_navigation/__init__.py
@@ -21,7 +21,7 @@ bl_info = {
     "name": "Mouselook Navigation",
     "description": "Alternative 3D view navigation",
     "author": "dairin0d",
-    "version": (1, 0, 0),
+    "version": (1, 0, 1),
     "blender": (2, 7, 0),
     "location": "View3D > orbit/pan/dolly/zoom/fly/walk",
     "warning": "",
